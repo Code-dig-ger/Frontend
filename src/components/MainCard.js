276d46ec0d7874c35ed54efa7aab1802543e060a
Sai Keshari <saikeshari@gmail.com>
2020-12-21 15:07:59 +0530

    Question Cards Added

--- src/components/MainCard.js
+++ src/components/MainCard.js
@@ -8,23 +8,50 @@ import './MainCard.css';
 
 
 const MainCard = (props) => {
+  if(props.buttonDis){
+    return(
+      <>
+      <div className="card">
+      <h3 className="title">{props.ProblemData.name}</h3>
+      <div className="bar">
+        <div className="emptybar" />
+        <div className={props.ProblemData.status === "solved"? "filledbar": "exapmplebar"}></div>
+      </div>
+        <div className="circle">
+          <svg version="1.1" xmlns="http://www.w3.org/2000/svg">
+            <circle className="stroke" cx="60" cy="60" r="50"/>
+          </svg>
+        </div>
+        <div className="container_card">
+          <Button className="buttondisp" href={props.ProblemData.url}>Solve</Button>
+        </div>
+      </div>
+    </>
+    )
+  }
+  else{
   return(
     <>
-      <div class="card">
-      <h3 class="title">Card 1</h3>
-      <div class="bar">
-        <div class="emptybar"></div>
-        <div class="filledbar"></div>
+      <div className="card">
+      <h3 className="title">{props.ProblemData.name}</h3>
+      <div className="bar">
+        <div className="emptybar" />
+        <div className={props.ProblemData.status === "solved"? "filledbar": ""}></div>
       </div>
-        <div class="circle">
+        <div className="circle">
           <svg version="1.1" xmlns="http://www.w3.org/2000/svg">
-            <circle class="stroke" cx="60" cy="60" r="50"/>
+            <circle className="stroke" cx="60" cy="60" r="50"/>
           </svg>
+        </div>
+        <div className="container_card">
+          <div className={props.count > 1 ? "lock": "check"}></div>
         </div>
       </div>
     </>
   )
+  }
 };
+
 
 
 export default MainCard;
--- src/pages/ladders/LaddersQuestionPage.js
+++ src/pages/ladders/LaddersQuestionPage.js
@@ -7,18 +7,29 @@ import './LaddersQuestionPage.css';
 import problemsData from './problemsData.json';
 
 function LaddersQuestionPage() {
-
+    var count=0;
+    var button = false;
     return (
         <div>
             <Navbar />
             <div className="container-fluid">
                 <div className="container-card">
                     {problemsData.result.map((ProblemData)=>{
-                        console.log("sai");
+                        if(ProblemData.status === "unsolved")
+                        {
+                            count++;
+                            if(count === 1){
+                                button = true;
+                            }
+                            else{
+                                button=false;
+                            }
+                            
+                        }
                         return(
                             <>
                                 
-                                    <MainCard />
+                                    <MainCard count={count} ProblemData={ProblemData} buttonDis={button}/>
                             </>
                         )
                         
