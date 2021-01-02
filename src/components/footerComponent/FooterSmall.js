0bf02552596f227d2b8da700e3b2dbf6cfcfbaca
Anurag5086 <anuraggupta5086@gmail.com>
2021-01-02 16:30:43 +0530

    footer

--- src/components/footerComponent/FooterSmall.js
+++ src/components/footerComponent/FooterSmall.js
@@ -10,8 +10,8 @@ import image1 from './images/codeforces.jpg';
 
 function FooterSmall() {
     return(
-    <div className="footer m-0 p-0">
-        <div className="container-fluid p-0 m-0">
+    <div className="footer m-0">
+        <div className="container-fluid p-4 m-0">
             <div className="row justify-content-center"> 
                             
                 <div className="col-6 col-sm-6">
