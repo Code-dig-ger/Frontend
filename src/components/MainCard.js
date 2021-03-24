7fc4e0bd12db0824bbd4d0a02c48b14799627125
Sai Keshari <saikeshari@gmail.com>
2021-03-24 20:22:33 +0530

    Add to playlist added

--- src/components/MainCard.js
+++ src/components/MainCard.js
@@ -18,20 +18,15 @@ const MainCard = (props) => {
   const [modal, setModal] = useState(false);
   const [playlists, setPlaylists] = useState([]);
   const [error, setErrors] = useState(false);
+  const [problemAdd, setProblemAdd] = useState({});
 
   useEffect(() => {
     console.log(modal);
   })
 
-  async function toggle(event) {
-    event.preventDefault();
-    setModal(!modal);
-    console.log(modal);
-    
-    if(!modal)
-    {
-      console.log("ppppppp");
-      const res = await fetch(`https://api.codedigger.tech/lists/userlist/`, {
+  async function getPlaylists()
+  {
+    const res = await fetch(`https://api.codedigger.tech/lists/userlist/`, {
             method:"GET",
             headers:{
                 "Content-Type":"application/json",
@@ -42,10 +37,56 @@ const MainCard = (props) => {
                 .json()
                 .then(res => setPlaylists(res))
                 .catch(error => setErrors(true));
+  }
+
+  function toggle(event) {
+    event.preventDefault();
+    setModal(!modal);
+    console.log(modal);
+    
+    if(!modal)
+    {
+      console.log("ppppppp");
+      getPlaylists();
       // fetchData();
     }
     console.log(playlists);
   };
+
+  function addProblem(slug, prob_id, platform){
+    let p;
+    if(platform === "Codeforces"){
+      p = "F";
+    }else if(platform === "Codechef"){
+      p = "C";
+    }else if(platform === "Atcoder"){
+      p = "A";
+    }else if(platform === "UVA"){
+      p = "U";
+    }else if(platform === "SPOJ"){
+      p = "S";
+    }
+
+    console.log(slug, prob_id, platform)
+      fetch (`https://api.codedigger.tech/lists/userlist/add`,{
+          method:"POST",
+          headers:{
+              "Content-type":"application/json",
+              "Authorization":`Bearer ${creds.access}`
+          },
+          body:JSON.stringify({
+              "prob_id": prob_id,
+              "slug": slug,
+              "platform": p
+          })
+      }).then(data => setProblemAdd(data));
+      
+      if(problemAdd.status == 200){
+        alert("Problem Added")
+      }else{
+        alert("Problem already added")
+      }
+  }
 
   if(props.type === "ladder")
   {
@@ -58,7 +99,7 @@ const MainCard = (props) => {
             placement="top"
             overlay={renderTooltip1}
           >
-            <a onClick={toggle} style={{cursor:"pointer"}}><FontAwesomeIcon icon={faFolderPlus} /></a>
+            <span onClick={toggle} ><FontAwesomeIcon style={{cursor:"pointer"}} icon={faFolderPlus} /></span>
           </OverlayTrigger></span>
           <Modal isOpen={modal} toggle={toggle}>
         <ModalHeader toggle={toggle}>Add to Playlist</ModalHeader>
@@ -71,6 +112,7 @@ const MainCard = (props) => {
                 <li>
                   <span style={{color:"white", fontSize:"19px"}}>{list.name}</span>
                   <Button 
+                            onClick={() => {addProblem(list.slug, props.ProblemData.prob_id, props.ProblemData.platform)}}
                             color="success" 
                             style={{padding:"5px 7px", 
                             position:"relative", 
@@ -115,8 +157,38 @@ const MainCard = (props) => {
             placement="top"
             overlay={renderTooltip1}
           >
-            <a href=""><FontAwesomeIcon icon={faFolderPlus} /></a>
+            <span onClick={toggle} ><FontAwesomeIcon style={{cursor:"pointer"}} icon={faFolderPlus} /></span>
           </OverlayTrigger></span>}
+          <Modal isOpen={modal} toggle={toggle}>
+        <ModalHeader toggle={toggle}>Add to Playlist</ModalHeader>
+        <ModalBody>
+        </ModalBody>
+          <ul>
+          {playlists.map((list) => {
+            return(
+              <>
+                <li>
+                  <span style={{color:"white", fontSize:"19px"}}>{list.name}</span>
+                  <Button 
+                            onClick={() => {addProblem(list.slug, props.ProblemData.prob_id, props.ProblemData.platform)}}
+                            color="success" 
+                            style={{padding:"5px 7px", 
+                            position:"relative", 
+                            left:"20px", 
+                            bottom:"0",
+                            borderRadius:"10%"}}>
+                              Add
+                            </Button>
+                            
+                </li>
+              </>
+            )
+          })}
+          </ul>
+        <ModalFooter>
+          <Button color="secondary" onClick={toggle}>Close</Button>
+        </ModalFooter>
+      </Modal>
         <h6 className={(props.count > props.solvedBtn && props.solvedBtn!=-1) ? "title_hide" : "mt-5 ml-3 pl-1"}>Platform: {props.ProblemData.platform}</h6>
         <h3 className={(props.count > props.solvedBtn && props.solvedBtn!=-1) ? "title_locked" : "title_hide"}>?</h3>
         <div className="bar">
@@ -150,8 +222,38 @@ const MainCard = (props) => {
             placement="top"
             overlay={renderTooltip1}
           >
-            <a href=""><FontAwesomeIcon icon={faFolderPlus} /></a>
+            <span onClick={toggle} ><FontAwesomeIcon style={{cursor:"pointer"}} icon={faFolderPlus} /></span>
           </OverlayTrigger></span>
+          <Modal isOpen={modal} toggle={toggle}>
+        <ModalHeader toggle={toggle}>Add to Playlist</ModalHeader>
+        <ModalBody>
+        </ModalBody>
+          <ul>
+          {playlists.map((list) => {
+            return(
+              <>
+                <li>
+                  <span style={{color:"white", fontSize:"19px"}}>{list.name}</span>
+                  <Button 
+                            onClick={() => {addProblem(list.slug, props.ProblemData.prob_id, props.ProblemData.platform)}}
+                            color="success" 
+                            style={{padding:"5px 7px", 
+                            position:"relative", 
+                            left:"20px", 
+                            bottom:"0",
+                            borderRadius:"10%"}}>
+                              Add
+                            </Button>
+                            
+                </li>
+              </>
+            )
+          })}
+          </ul>
+        <ModalFooter>
+          <Button color="secondary" onClick={toggle}>Close</Button>
+        </ModalFooter>
+      </Modal>
         <h6 className="mt-5 ml-3 pl-1">Platform: {props.ProblemData.platform}</h6>
         <div className="bar">
           <div className="emptybar" />
@@ -181,8 +283,38 @@ const MainCard = (props) => {
             placement="top"
             overlay={renderTooltip1}
           >
-            <a href=""><FontAwesomeIcon icon={faFolderPlus} /></a>
+            <span onClick={toggle} ><FontAwesomeIcon style={{cursor:"pointer"}} icon={faFolderPlus} /></span>
           </OverlayTrigger></span>
+          <Modal isOpen={modal} toggle={toggle}>
+        <ModalHeader toggle={toggle}>Add to Playlist</ModalHeader>
+        <ModalBody>
+        </ModalBody>
+          <ul>
+          {playlists.map((list) => {
+            return(
+              <>
+                <li>
+                  <span style={{color:"white", fontSize:"19px"}}>{list.name}</span>
+                  <Button 
+                            onClick={() => {addProblem(list.slug, props.ProblemData.prob_id, props.ProblemData.platform)}}
+                            color="success" 
+                            style={{padding:"5px 7px", 
+                            position:"relative", 
+                            left:"20px", 
+                            bottom:"0",
+                            borderRadius:"10%"}}>
+                              Add
+                            </Button>
+                            
+                </li>
+              </>
+            )
+          })}
+          </ul>
+        <ModalFooter>
+          <Button color="secondary" onClick={toggle}>Close</Button>
+        </ModalFooter>
+      </Modal>
         <h6 className="mt-5 ml-3 pl-1">Platform: {props.ProblemData.platform}</h6>
         <div className="bar">
           <div className="emptybar" />
@@ -210,8 +342,38 @@ const MainCard = (props) => {
             placement="top"
             overlay={renderTooltip1}
           >
-            <a href=""><FontAwesomeIcon icon={faFolderPlus} /></a>
+            <span onClick={toggle} ><FontAwesomeIcon style={{cursor:"pointer"}} icon={faFolderPlus} /></span>
           </OverlayTrigger></span>
+          <Modal isOpen={modal} toggle={toggle}>
+        <ModalHeader toggle={toggle}>Add to Playlist</ModalHeader>
+        <ModalBody>
+        </ModalBody>
+          <ul>
+          {playlists.map((list) => {
+            return(
+              <>
+                <li>
+                  <span style={{color:"white", fontSize:"19px"}}>{list.name}</span>
+                  <Button 
+                            onClick={() => {addProblem(list.slug, props.ProblemData.prob_id, props.ProblemData.platform)}}
+                            color="success" 
+                            style={{padding:"5px 7px", 
+                            position:"relative", 
+                            left:"20px", 
+                            bottom:"0",
+                            borderRadius:"10%"}}>
+                              Add
+                            </Button>
+                            
+                </li>
+              </>
+            )
+          })}
+          </ul>
+        <ModalFooter>
+          <Button color="secondary" onClick={toggle}>Close</Button>
+        </ModalFooter>
+      </Modal>
         <h6 className="mt-5 ml-3 pl-1">Platform: {props.ProblemData.platform}</h6>
         <h3 className="title_hide">?</h3>
         <div className="bar">
