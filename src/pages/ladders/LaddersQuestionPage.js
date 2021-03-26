52e7a371faf0bd3f575a4e9247daf11c01663869
Anurag5086 <anuraggupta5086@gmail.com>
2021-03-26 16:45:23 +0400

    ladders page text fix

--- src/pages/ladders/LaddersQuestionPage.js
+++ src/pages/ladders/LaddersQuestionPage.js
@@ -106,7 +106,9 @@ function LaddersQuestionPage(props) {
       return (
         show==true ? <Loading/>:<div>
         <Navbar />
-        <h2 style={{textAlign:"center", marginTop: '120px'}}>{problems.meta.description}</h2>
+        <h2 style={{textAlign:"center", marginTop: '120px', fontSize: '45px'}}>{problems.meta.name}</h2>
+        <p style={{textAlign:"center", marginTop: '10px', fontSize: '17px'}}>{problems.meta.description}</p>
+        {console.log(problems.meta)}
         <Button variant="success" className="updateBtn" onClick={updateReload}>Update</Button>
         <div className="container-fluid" style={{display:"flex",flexDirection:"row",alignItems:"center",padding:"0rem 0rem"}}>
 
